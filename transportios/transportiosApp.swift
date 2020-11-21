ddce9638bec8a02093b2338c115b476e2658a2e1
acbriceno <acbriceno@live.com>
2020-11-21 16:26:38 -0330

    Initial Commit

---
+++ transportios/transportiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  transportiosApp.swift
+//  transportios
+//
+//  Created by grid on 2020-11-21.
+//
+
+import SwiftUI
+
+@main
+struct transportiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
